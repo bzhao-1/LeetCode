e04ed904bbdfafedff9651766bfb1f4521621c63
Ben Zhao <zhaob@carleton.edu>
2023-03-17 13:33:56 -0400

    updates

--- Python/704- Binary Search.py
+++ Python/704- Binary Search.py
@@ -1,5 +1,7 @@
 class Solution:
+    #Violates principles of clean code in that the function is too long, no more than 3-4 lines
     def search(self, nums: List[int], target: int) -> int:
+        #no more than 1 argument passed in violated
         left = 0
         right = len(nums)-1
         
@@ -12,4 +14,5 @@ class Solution:
             elif target > nums[n]:
                 left = n + 1 
         return -1
+        #however, no side missions, only does one thing!
 #O(log n) time && O(1) space 
